f6883df54a789d12ca551d8dbde4da4946f46c60
Justinator <32310311+Justinator@users.noreply.github.com>
2019-06-18 06:25:28 -0500

    added discount table and started building out bus stop details section

--- page-wisconsin-rapids-to-madison-and-milwaukee.php
+++ page-wisconsin-rapids-to-madison-and-milwaukee.php
@@ -110,6 +110,81 @@ get_header();
 						} ?>
 				</div>
 			</section>
+			<section class="stopDetails">
+				<div class="navWidth flex-container">
+					<div class="col50 faresTable">
+						<?php
+			        $image = get_field('route_map');
+			        $imageID = $image['ID'];
+			        echo wp_get_attachment_image( $imageID, 'full', false, array( 'class' => 'image', 'data-sizes' => 'auto' ) );
+			      ?>
+						<?php $table = get_field( 'discount_table' );
+							if ( ! empty ( $table ) ) {
+							    echo '<table border="0">';
+							        if ( ! empty( $table['caption'] ) ) {
+							            echo '<caption>' . $table['caption'] . '</caption>';
+							        }
+							        if ( ! empty( $table['header'] ) ) {
+							            echo '<thead>';
+							                echo '<tr>';
+							                    foreach ( $table['header'] as $th ) {
+							                        echo '<th>';
+							                            echo $th['c'];
+							                        echo '</th>';
+							                    }
+							                echo '</tr>';
+							            echo '</thead>';
+							        }
+							        echo '<tbody>';
+							            foreach ( $table['body'] as $tr ) {
+							                echo '<tr>';
+							                    foreach ( $tr as $td ) {
+							                        echo '<td>';
+							                            echo $td['c'];
+							                        echo '</td>';
+							                    }
+							                echo '</tr>';
+							            }
+							        echo '</tbody>';
+							    echo '</table>';
+							} ?>
+					</div>
+					<div class="col50 textWrap textBlock">
+						<h3 class="largeHeading"><span class="underline"><?php the_field('section_header'); ?></span></h3>
+						<div class="fullWidth stopDetailsWrap">
+							<div class="col50">
+								<?php if( get_field('stop_1_city') ): ?><p><?php the_field('stop_1_city'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_1_address') ): ?><p><?php the_field('stop_1_address'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_2') ): ?><p><?php the_field('stop_2'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_3') ): ?><p><?php the_field('stop_3'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_4') ): ?><p><?php the_field('stop_4'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_5') ): ?><p><?php the_field('stop_5'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_6') ): ?><p><?php the_field('stop_6'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_7') ): ?><p><?php the_field('stop_7'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_8') ): ?><p><?php the_field('stop_8'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_9') ): ?><p><?php the_field('stop_9'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_10') ): ?><p><?php the_field('stop_10'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_11') ): ?><p><?php the_field('stop_11'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_12') ): ?><p><?php the_field('stop_12'); ?></p><?php endif; ?>
+							</div>
+							<div class="col50">
+								<?php if( get_field('stop_1_map') ): ?><p><?php the_field('stop_1_map'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_2_map') ): ?><p><?php the_field('stop_2_map'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_3_map') ): ?><p><?php the_field('stop_3_map'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_4_map') ): ?><p><?php the_field('stop_4_map'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_5_map') ): ?><p><?php the_field('stop_5_map'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_6_map') ): ?><p><?php the_field('stop_6_map'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_7_map') ): ?><p><?php the_field('stop_7_map'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_8_map') ): ?><p><?php the_field('stop_8_map'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_9_map') ): ?><p><?php the_field('stop_9_map'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_10_map') ): ?><p><?php the_field('stop_10_map'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_11_map') ): ?><p><?php the_field('stop_11_map'); ?></p><?php endif; ?>
+								<?php if( get_field('stop_12_map') ): ?><p><?php the_field('stop_12_map'); ?></p><?php endif; ?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 		</section>
 	</main>
 </div>
