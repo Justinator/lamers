32bf08317ba799e91fe3861be16869c2b1455ed6
Justin-Insight <jparsons@insightcreative.com>
2019-07-17 10:02:01 -0500

    updated testimonials section to be a global block, combined accordion.js with customJS, added scroll indicators for mobile device users on the connect pages

--- functions.php
+++ functions.php
@@ -182,12 +182,26 @@ function insightcustom_resource_hints( $urls, $relation_type ) {
 }
 add_filter( 'wp_resource_hints', 'insightcustom_resource_hints', 10, 2 );
 
+// Advanced Custom Fields Customizations
 // Add ACF options page for global settings
 if( function_exists('acf_add_options_page') ) {
-
 	acf_add_options_page();
-
 }
+// Set fields to collapsed for a condensed view for new users
+function my_acf_admin_head() {
+    ?>
+    <script type="text/javascript">
+        (function($){
+            $(document).ready(function(){
+                $('.layout').addClass('-collapsed');
+                $('.acf-postbox').addClass('closed');
+            });
+        })(jQuery);
+    </script>
+    <?php
+}
+add_action('acf/input/admin_head', 'my_acf_admin_head');
+
 /**
  * Enqueue scripts and styles.
  */
--- page-connect.php
+++ page-connect.php
@@ -83,19 +83,7 @@ get_header();
 			</section>
 			<section class="routeRates paddedSection">
 				<div class="pageWidth centerText">
-					<h4 class="largeHeading"><?php the_field('fares_title'); ?></h4>
-					<div id="scrollWrap">
-						<p class="noMargin">Scroll to view full table of rates</p>
-						<svg class="smallIcon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-							<defs>
-								<symbol id="icon-arrows-h" viewBox="0 0 28 28">
-									<title>arrows-h</title>
-									<path d="M28 14c0 0.266-0.109 0.516-0.297 0.703l-4 4c-0.187 0.187-0.438 0.297-0.703 0.297-0.547 0-1-0.453-1-1v-2h-16v2c0 0.547-0.453 1-1 1-0.266 0-0.516-0.109-0.703-0.297l-4-4c-0.187-0.187-0.297-0.438-0.297-0.703s0.109-0.516 0.297-0.703l4-4c0.187-0.187 0.438-0.297 0.703-0.297 0.547 0 1 0.453 1 1v2h16v-2c0-0.547 0.453-1 1-1 0.266 0 0.516 0.109 0.703 0.297l4 4c0.187 0.187 0.297 0.438 0.297 0.703z"></path>
-								</symbol>
-							</defs>
-						</svg>
-						<svg id="redArrow" class="redIcon icon-arrows-h"><use xlink:href="#icon-arrows-h"></use></svg>
-					</div>
+					<h4 class="largeHeading"><span class="underline"><?php the_field('fares_title'); ?></span></h4>
 				</div>
 				<div class="navWidth faresTable">
 					<?php $table = get_field( 'fares_table' );
@@ -189,6 +177,6 @@ get_header();
 		</section>
 	</main>
 </div>
-<script src="/wp-content/themes/insightCustom/js/customJS.js"></script>
+<script src="/wp-content/themes/insightCustom/js/accordion.js"></script>
 <?php
 get_footer();
