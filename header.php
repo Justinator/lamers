13a150a794032dd8189c8706055e9aa0cf94d2d8
Justin-Insight <jparsons@insightcreative.com>
2020-01-28 09:12:44 -0600

    removed the hotjar tracking code from the header of the site

--- header.php
+++ header.php
@@ -14,17 +14,11 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
 	<script src="/wp-content/themes/insightCustom/js/slick.js"></script>
 	<link rel="stylesheet" href="https://use.typekit.net/mkv6ofm.css">
-	<!-- Hotjar Tracking Code for https://wordpress-154794-852975.cloudwaysapps.com/ -->
-	<script>
-	    (function(h,o,t,j,a,r){
-	        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-	        h._hjSettings={hjid:1455347,hjsv:6};
-	        a=o.getElementsByTagName('head')[0];
-	        r=o.createElement('script');r.async=1;
-	        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-	        a.appendChild(r);
-	    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-	</script>
+	<!-- Preconnect to Google Analytics -->
+	<link rel="dns-prefetch" href="https://www.google-analytics.com">
+	<link rel="dns-prefetch" href="https://www.googletagmanager.com">
+	<link href="https://www.google-analytics.com" rel="preconnect" crossorigin>
+	<link href="https://www.googletagmanager.com" rel="preconnect" crossorigin>
 	<!-- Google Analytics -->
 	<script>
 	window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
